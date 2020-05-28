07499ca7fc6d0cb3acb9435501000d1336b9af3c
heathergi85 <heathergi85@gmail.com>
2020-05-28 09:44:27 +0000

    shuffle function added

--- assets/js/tiles.js
+++ assets/js/tiles.js
@@ -1,26 +1,35 @@
+
+
 var tileImages = ['tile1.jpg', 'tile2.jpg', 'tile3.jpg', 'tile4.jpg', 'tile5.jpg', 'tile6.jpg'];
-    var gameboard = document.getElementById("gameboard");
-    console.log(tileImages);
-    startGame();
-    function startGame() {
+var gameboard = document.getElementById("gameboard");
+
+var solutionArray = tileImages.concat(tileImages);
+console.log(solutionArray);
+shuffleArray(solutionArray);
+console.log(solutionArray);
+
+
+///console.log(tileImages);
+startGame();
+function startGame() {
         gameboard.innerHTML="";
 
-        for (var i=0; i<=((tileImages.length*2)-1); i++){
+        for (var i=0; i<=((solutionArray.length)-1); i++){
             displayImage(i)
         }
-    }
+}
 
-    function displayImage(i) {
-    console.log(i);
-    gameboard.innerHTML += '<div class="col-md-4 col-xs-3 gametile">' + i + '</div>';
-    }
+function displayImage(i) {
 
-    function shuffleArray(d) {
-        for (var c = d.length - 1; c > 0; c--) {
-            var b = Math.floor(Math.random() * (c + 1));
-            var a = d[c];
-            d[c] = d[b];
-            d[b] = a;
-    }
-return d
+gameboard.innerHTML += '<div class="col-md-4 col-xs-3 gametile">' + i + '</div>';
+}
+
+function shuffleArray(d) {
+            for (var c = d.length - 1; c > 0; c--) {
+                var b = Math.floor(Math.random() * (c + 1));
+                var a = d[c];
+                d[c] = d[b];
+                d[b] = a;
+            }
+            return d
 };
