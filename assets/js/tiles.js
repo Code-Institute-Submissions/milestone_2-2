dbf72f49d2cd28ce095dbca1c6b78cb113261e26
heathergi85 <heathergi85@gmail.com>
2020-05-30 06:21:09 +0000

    card flip, picking, matching written

--- assets/js/tiles.js
+++ assets/js/tiles.js
@@ -2,8 +2,10 @@
 
 var tileImages = ['tile1.jpg', 'tile2.jpg', 'tile3.jpg', 'tile4.jpg', 'tile5.jpg', 'tile6.jpg'];
 var gameboard = document.getElementById("gameboard");
-
+var cardsflippedover = 0,lastcardpicked = -1,timer = '',score = 0;
 var solutionArray = tileImages.concat(tileImages);
+fliparray = new Array();
+
 console.log(solutionArray);
 shuffleArray(solutionArray);
 console.log(solutionArray);
@@ -21,8 +23,55 @@ function startGame() {
 
 function displayImage(i) {
 
-gameboard.innerHTML += '<div class="col-md-4 col-xs-3 gametile">' + i + '</div>';
+gameboard.innerHTML += '<div class="col-md-4 col-xs-3 gametile"><img src="assets/img/back.jpg" onclick="pickCard(\''+solutionArray[i]+'\',\'+ i +\', this);return false;" class="flipImage"></div>';
 }
+
+
+function pickCard(a, b, c) {
+            if (cardsflippedover < 2 && lastcardpicked != b) {
+                fliparray[cardsflippedover] = solutionArray[b];
+                fliparray[(cardsflippedover + 2)] = c.id;
+                cardsflippedover++;
+                c.src = "#" + solutionArray[b];
+                if (cardsflippedover == 2) {
+                    if (fliparray[0] == fliparray[1]) {
+                        messageText("Match FOUND");
+                        console.log('same');
+                        pickagain();
+                        score++;
+                        if (tileImages.length <= score) {
+                            console.log('END GAME');
+                            gameDone();
+                        }
+                    }
+                    else {
+                        timer = setInterval(hideCard, 3000);
+                        console.log('different');
+                        messageText("NOT FOUND");
+                    }
+                }
+                lastcardpicked = b;
+            }
+}
+
+function pickagain() {
+            cardsflippedover = 0;
+            fliparray = [];
+            lastcardpicked = -1;
+            clearInterval(timer);
+}
+
+function hideCard() {
+            console.log(fliparray);
+            if (fliparray[2]) {
+                document.getElementById(fliparray[2]).src = "img/back.jpg";
+            }
+            if (fliparray[3]) {
+                document.getElementById(fliparray[3]).src = "img/back.jpg";
+            }
+            pickagain();
+}
+
 
 function shuffleArray(d) {
             for (var c = d.length - 1; c > 0; c--) {
