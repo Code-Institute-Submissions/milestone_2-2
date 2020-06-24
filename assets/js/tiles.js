bfd03c43a92b9c08f94a3f328b26ea3f913d4d8e
heathergi85 <heathergi85@gmail.com>
2020-06-24 19:59:38 +0000

    footer styling, page styling

--- assets/js/tiles.js
+++ assets/js/tiles.js
@@ -3,33 +3,23 @@ var tileImages = ['tile1.jpg', 'tile2.jpg', 'tile3.jpg', 'tile4.jpg', 'tile5.jpg
 var gameboard = document.getElementById("gameboard");
 var messageTop = document.getElementById("message");
 var buttonmessage = document.getElementById("gamecontrol");
-var mytime = document.getElementById("mytime");
+
 var cardsflippedover = 0
             , lastcardpicked = -1
-            , timer = ''
-            , score = 0
-            , mess = ''
-            , seconds = 0
-            , mseconds = 0
-            , minutes = 0
-            , hours = 0
-            , t, gamescore = 100;
+            
+            
+            
+            
 var solutionArray = tileImages.concat(tileImages);
         document.getElementById("gamecontrol").addEventListener("click", startGame);
         fliparray = new Array();
         startGame();
 
 function startGame() {
-            clearInterval(timer);
-            timerX();
-            seconds = 0, mseconds = 0, minutes = 0, hours = 0, gamescore = 100;
             shuffleArray(solutionArray);
-            score = 0;
             gameboard.innerHTML = "";
-            buttonmessage.innerHTML = "Restart Game";
-            messageText("Click a Tile to start");
             for (var i = 0; i <= ((solutionArray.length) - 1); i++) {
-                gameboard.innerHTML += '<div class="col-md-3 col-xs-4 gametile"><img id="cardz' + i + '" src="img/back.jpg" onclick="pickCard(\'' + solutionArray[i] + '\',\'' + i + '\',this);return false;" class="flipimage"></div>';
+                gameboard.innerHTML += '<div class="col-md-3 col-xs-4 gametile"><img id="cardz' + i + '" src="assets/img/back.jpg" onclick="pickCard(\'' + solutionArray[i] + '\',\'' + i + '\',this);return false;" class="flipimage"></div>';
             }
 }
 
@@ -38,10 +28,9 @@ function pickCard(a, b, c) {
                 fliparray[cardsflippedover] = solutionArray[b];
                 fliparray[(cardsflippedover + 2)] = c.id;
                 cardsflippedover++;
-                c.src = 'img/' + solutionArray[b];
+                c.src = 'assets/img/' + solutionArray[b];
                 if (cardsflippedover == 2) {
                     if (fliparray[0] == fliparray[1]) {
-                        messageText("Match");
                         console.log('same');
                         pickagain();
                         score++;
@@ -53,7 +42,6 @@ function pickCard(a, b, c) {
                     else {
                         timer = setInterval(hideCard, 3000);
                         console.log('different');
-                        messageText("Try Again");
                     }
                 }
                 lastcardpicked = b;
@@ -61,38 +49,18 @@ function pickCard(a, b, c) {
 }
 
 function messageText(message) {
-            clearInterval(mess);
             console.log('message');
             messageTop.innerHTML = message;
             if (message != 'Find a match') {
-                mess = setInterval(messageText, 1000, 'Find a match');
+                mess = setInterval(messageText, 1000,);
             }
-}
-
-function addTime() {
-            gamescore--;
-            seconds++;
-            if (seconds >= 60) {
-                seconds = 0;
-                minutes++;
-                if (minutes >= 60) {
-                    minutes = 0;
-                    hours++;
-                }
-            }
-            mytime.textContent = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
-            timerX();
-}
-
-function timerX() {
-            t = setTimeout(addTime, 1000);
 }
 
 function gameDone() {
             if (gamescore < 0) {
                 gamescore = 0;
             }
-            messageText("GAME OVER<BR>THANKS for PLAYING<BR>You scored = " + gamescore);
+            messageText("GAME OVER<BR>THANKS for PLAYING<BR>");
             buttonmessage.innerHTML = "Click to Play Again";
 }
 
@@ -100,16 +68,16 @@ function pickagain() {
             cardsflippedover = 0;
             fliparray = [];
             lastcardpicked = -1;
-            clearInterval(timer);
+            
 }
 
 function hideCard() {
             console.log(fliparray);
             if (fliparray[2]) {
-                document.getElementById(fliparray[2]).src = "img/back.jpg";
+                document.getElementById(fliparray[2]).src = "assets/img/back.jpg";
             }
             if (fliparray[3]) {
-                document.getElementById(fliparray[3]).src = "img/back.jpg";
+                document.getElementById(fliparray[3]).src = "assets/img/back.jpg";
             }
             pickagain();
 }
